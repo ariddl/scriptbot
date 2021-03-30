1e5014bc19ee93c943825c2fd5796591f03b3bfe
Alexander Riddle <amrwf8@mail.missouri.edu>
2021-03-29 20:51:43 -0700

    Utility: add AtomicConsole for async printouts

---
+++ src/Utility/AtomicConsole.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace DiscordScriptBot.Utility
+{
+    public static class AtomicConsole
+    {
+        private static SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
+
+        public static void Write(string format, params string[] args)
+            => Wrap(() => Console.Write(format, args));
+        public static void WriteLine(string format, params string[] args)
+            => Wrap(() => Console.WriteLine(format, args));
+
+        private static async void Wrap(Action action)
+        {
+            await _semaphore.WaitAsync();
+            action();
+            _semaphore.Release();
+        }
+    }
+}
