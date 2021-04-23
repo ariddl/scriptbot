08d7fc591c8c9353dfcf35b72ae2c812e557fb78
Alexander Riddle <amrwf8@mail.missouri.edu>
2021-04-23 14:54:55 -0700

    Remove unused singleton class

--- src/Utility/Singleton.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DiscordScriptBot.Utility
-{
-    public class Singleton<T> where T : new()
-    {
-        private static T _instance;
-
-        public static T Instance => _instance ?? (_instance = new T());
-    }
-}
