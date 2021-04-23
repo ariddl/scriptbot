6337731bc527c65aca4914203045abe17c61fde6
Alexander Riddle <amrwf8@mail.missouri.edu>
2021-04-23 14:54:25 -0700

    New ScriptBuilder, remove old

--- src/Script/ScriptBuilder.cs
+++ src/Script/ScriptBuilder.cs
@@ -1,80 +1,144 @@
-﻿using DiscordScriptBot.Expression;
+﻿using DiscordScriptBot.Builder;
 using System;
 using System.Collections.Generic;
+using static DiscordScriptBot.Script.ScriptInterface;
 
 namespace DiscordScriptBot.Script
 {
     public class ScriptBuilder
     {
-        public class BuildException : Exception
+        private class BuilderException : Exception
         {
-            public BuildException(string function, string message)
-                : base($"ScriptBuilder.{function}() {message}")
+            public BuilderException(string func, string msg)
+                : base($"ScriptBuilder.{func}(): {msg}")
             {
             }
         }
 
-        private string _name;
-        private string _description;
-        private Stack<IScriptNode> _nodeStack;
-
-        public ScriptBuilder(string name, string description)
+        public struct CallInfo
         {
-            _name = name;
-            _description = description;
-            _nodeStack = new Stack<IScriptNode>();
-            Push<Script.ActionsNode>();
+            public string Ref { get; set; }
+            public string ClassName { get; set; }
+            public string ParamName { get; set; }
+            public string FuncName { get; set; }
+            public IExpression[] FuncParams { get; set; }
         }
 
-        public void Action(Action a) => Peek<Script.ActionsNode>().Actions.Add(IScriptNode.Wrap(a));
+        public static CallInfo CallByRef(string @ref, string func, IExpression[] @params)
+            => new CallInfo { Ref = @ref, FuncName = func, FuncParams = @params };
+        public static CallInfo CallByParam(string param, string func, IExpression[] @params)
+            => new CallInfo { ParamName = param, FuncName = func, FuncParams = @params };
 
-        public void Condition(IEvaluable cond)
+        public string Name { get; }
+        public string Description { get; }
+        public IWrapperInfo SourceEvent { get; }
+
+        private Stack<IExpression> _exprStack;
+
+        public ScriptBuilder(string name, string description, IWrapperInfo @event)
         {
-            var actionsNode = Peek<Script.ActionsNode>();
-            var conditionNode = Push<Script.ConditionNode>();
-            conditionNode.Condition = cond;
-            actionsNode.Actions.Add(conditionNode);
+            Name = name;
+            Description = description;
+            SourceEvent = @event;
+            _exprStack = new Stack<IExpression>();
+            Push<BlockExpression>();
         }
 
-        public void ConditionPassed() => Peek<Script.ConditionNode>().Passed = Push<Script.ActionsNode>();
-        public void ConditionFailed() => Peek<Script.ConditionNode>().Failed = Push<Script.ActionsNode>();
-
-        private T Push<T>() where T : IScriptNode, new()
+        private T Push<T>() where T : IExpression, new()
         {
-            T n = new T();
-            _nodeStack.Push(n);
-            return n;
+            T t = new T();
+            _exprStack.Push(t);
+            return t;
         }
 
-        public void Pop()
-        {
-            Assert(_nodeStack.Count > 1, "Pop", "cannot pop root node!");
-            _nodeStack.Pop();
-        }
-
-        private T Peek<T>() where T : IScriptNode
+        private T Peek<T>() where T : IExpression
         {
             string typeName = typeof(T).Name;
-            Assert(_nodeStack.Count > 0, $"Peek<{typeName}>", "stack is empty! (already built?)");
-            Assert(_nodeStack.Peek() is T, $"Peek<{typeName}>", $"top node not a(n) {typeName}!");
-            return (T)_nodeStack.Peek();
+            Assert(_exprStack.Count > 0, $"Peek<{typeName}>", "stack empty!");
+            Assert(_exprStack.Peek() is T, $"Peek<{typeName}>", "not at top of stack!");
+            return (T)_exprStack.Peek();
         }
 
-        public Script Build()
+        private T Pop<T>() where T : IExpression
         {
-            Assert(_nodeStack.Count > 0, "Build", "already built!");
-            while (_nodeStack.Count > 1)
-                _nodeStack.Pop();
-
-            var script = new Script(_name, _description, _nodeStack.Peek());
-            _nodeStack.Clear();
-            return script;
+            T expr = Peek<T>();
+            _exprStack.Pop();
+            return expr;
         }
 
-        private static void Assert(bool condition, string function, string message)
+        private void AppendToBlock(IExpression e) => Peek<BlockExpression>().Expressions.Add(e);
+        private void AppendToBlock<T>() where T : IExpression, new()
+            => Peek<BlockExpression>().Expressions.Add(Push<T>());
+
+        public void If(CallInfo info)
         {
-            if (!condition)
-                throw new BuildException(function, message);
+            AppendToBlock<IfExpression>();
+            Peek<IfExpression>().Test = ResolveCall(info);
+        }
+
+        public void Else()
+        {
+            Pop<BlockExpression>();
+            Peek<IfExpression>().IfFalse = Push<BlockExpression>();
+        }
+
+        public void And(CallInfo info) => ChainConditional(new AndExpression(), ResolveCall(info));
+
+        public void Or(CallInfo info) => ChainConditional(new OrExpression(), ResolveCall(info));
+
+        public void Then() => Peek<IfExpression>().IfTrue = Push<BlockExpression>();
+
+        public bool End() => Pop<BlockExpression>() != null && Pop<IfExpression>() != null;
+
+        public void Action(CallInfo info) => AppendToBlock(ResolveCall(info));
+
+        public BlockExpression Finish()
+        {
+            while (_exprStack.Count > 1)
+                _exprStack.Pop();
+            return Peek<BlockExpression>();
+        }
+
+        private CallExpression ResolveCall(CallInfo info)
+        {
+            string refType = CallExpression.ClassRef.TypeParam;
+            if (info.ClassName == null)
+            {
+                // Events only have properties (wrappers) which act as parameters.
+                // These properties contain all callable functions.
+                if (!SourceEvent.Properties.ContainsKey(info.ParamName))
+                    return null;
+                info.ClassName = SourceEvent.Properties[info.ParamName].Attribute.Name;
+            }
+            else
+                refType = ulong.TryParse(info.Ref, out ulong _)
+                    ? CallExpression.ClassRef.TypeId : CallExpression.ClassRef.TypeStr;
+
+            return new CallExpression
+            {
+                ClassName = info.ClassName,
+                FuncName = info.FuncName,
+                Parameters = info.FuncParams,
+                Ref = new CallExpression.ClassRef
+                {
+                    RefType = refType,
+                    Value = info.Ref
+                }
+            };
+        }
+        
+        private void ChainConditional(IBinaryExpression e, IExpression cond)
+        {
+            var @if = Peek<IfExpression>();
+            e.Left = @if.Test;
+            e.Right = cond;
+            @if.Test = e;
+        }
+
+        private static void Assert(bool cond, string func, string msg)
+        {
+            if (!cond)
+                throw new BuilderException(func, msg);
         }
     }
 }
