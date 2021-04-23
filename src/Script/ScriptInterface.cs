f37a623e1243ed35c8ba836110a425adf4e1e27a
Alexander Riddle <amrwf8@mail.missouri.edu>
2021-04-23 16:50:13 -0700

    ScriptInterface: a bit of clean up

--- src/Script/ScriptInterface.cs
+++ src/Script/ScriptInterface.cs
@@ -57,74 +57,77 @@ namespace DiscordScriptBot.Script
             {
                 // Iterate over all the types in the current assembly.
                 foreach (Type type in assembly.GetTypes())
+                    LoadType(type);
+            }
+        }
+
+        private void LoadType(Type type)
+        {
+            // Event/wrapper types must be marked by Wrapper attributes.
+            WrapperDecl wrapAttr;
+            if ((wrapAttr = type.GetCustomAttribute<WrapperDecl>()) == null)
+                return;
+
+            // Check if the type implements either the IWrapper or IEvent interfaces (required).
+            var interfaces = type.GetInterfaces();
+            if (!interfaces.Contains(typeof(IWrapper)) && !interfaces.Contains(typeof(IEvent)))
+                return;
+
+            // Events are marked by their implementation of IEvent. Choose the appropriate
+            // dictionary depending on if this is a wrapper or event type.
+            bool isEvent = interfaces.Contains(typeof(IEvent));
+            var wrapDict = !isEvent ? _wrappers : _events;
+            Debug.Assert(!wrapDict.ContainsKey(wrapAttr.Name),
+                $"Duplicate Wrapper Type: {type.Name} Name: {wrapAttr.Name}");
+
+            // Initialize the wrapper info to hold information about the APIs exposed by this type.
+            var wrapper = new WrapperInfo
+            {
+                Name = wrapAttr.Name,
+                Description = wrapAttr.Description,
+                Type = type,
+                Actions = new Dictionary<string, FunctionInfo>(),
+                Conditionals = new Dictionary<string, FunctionInfo>(),
+                Properties = new Dictionary<string, FunctionInfo>()
+            };
+            wrapDict.Add(wrapAttr.Name, wrapper);
+            _wrapperTypes.Add(type, wrapper);
+
+            // Iterate over the members of the type to find the API functions exposed by this type.
+            foreach (MemberInfo m in type.GetMembers())
+            {
+                // Each API member must be marked by a WrapperDecl, similar to the type itself.
+                WrapperDecl memberAttr;
+                if ((memberAttr = m.GetCustomAttribute<WrapperDecl>()) == null)
+                    continue;
+
+                // Support only methods for now. We may want to explore supporting properties as well...
+                Debug.Assert(m.MemberType == MemberTypes.Method,
+                    $"Interface member is not a method: {memberAttr.Name}");
+                MethodInfo method = (MethodInfo)m;
+
+                // Choose the appropriate dictionary to store this function
+                var rt = method.ReturnType;
+                var dict = wrapper.Properties;
+                if (rt == typeof(bool))
+                    dict = wrapper.Conditionals;
+                else if (rt == typeof(void) || rt == typeof(Task))
+                    dict = wrapper.Actions;
+
+                Debug.Assert(!isEvent || dict != wrapper.Conditionals, "Event has conditional!");
+                Debug.Assert(!dict.ContainsKey(memberAttr.Name),
+                    $"Duplicate interface member: {memberAttr.Name}");
+
+                // Build the function info and function parameter dictionary.
+                var parameters = new List<(string, string)>();
+                foreach (ParameterInfo parameter in method.GetParameters())
+                    parameters.Add((parameter.Name, parameter.ParameterType.Name));
+                dict.Add(memberAttr.Name, new FunctionInfo
                 {
-                    // Event/wrapper types must be marked by Wrapper attributes.
-                    WrapperDecl wrapAttr;
-                    if ((wrapAttr = type.GetCustomAttribute<WrapperDecl>()) == null)
-                        continue;
-
-                    // Check if the type implements either the IWrapper or IEvent interfaces (required).
-                    var interfaces = type.GetInterfaces();
-                    if (!interfaces.Contains(typeof(IWrapper)) && !interfaces.Contains(typeof(IEvent)))
-                        continue;
-
-                    // Events are marked by their implementation of IEvent. Choose the appropriate
-                    // dictionary depending on if this is a wrapper or event type.
-                    bool isEvent = interfaces.Contains(typeof(IEvent));
-                    var wrapDict = !isEvent ? _wrappers : _events;
-                    Debug.Assert(!wrapDict.ContainsKey(wrapAttr.Name),
-                        $"Duplicate Wrapper Type: {type.Name} Name: {wrapAttr.Name}");
-
-                    // Initialize the wrapper info to hold information about the APIs exposed by this type.
-                    var wrapper = new WrapperInfo
-                    {
-                        Name = wrapAttr.Name,
-                        Description = wrapAttr.Description,
-                        Type = type,
-                        Actions = new Dictionary<string, FunctionInfo>(),
-                        Conditionals = new Dictionary<string, FunctionInfo>(),
-                        Properties = new Dictionary<string, FunctionInfo>()
-                    };
-                    wrapDict.Add(wrapAttr.Name, wrapper);
-                    _wrapperTypes.Add(type, wrapper);
-
-                    // Iterate over the members of the type to find the API functions exposed by this type.
-                    foreach (MemberInfo m in type.GetMembers())
-                    {
-                        // Each API member must be marked by a WrapperDecl, similar to the type itself.
-                        WrapperDecl memberAttr;
-                        if ((memberAttr = m.GetCustomAttribute<WrapperDecl>()) == null)
-                            continue;
-
-                        // Support only methods for now. We may want to explore supporting properties as well...
-                        Debug.Assert(m.MemberType == MemberTypes.Method,
-                            $"Interface member is not a method: {memberAttr.Name}");
-                        MethodInfo method = (MethodInfo)m;
-
-                        // Choose the appropriate dictionary to store this function
-                        var rt = method.ReturnType;
-                        var dict = wrapper.Properties;
-                        if (rt == typeof(bool))
-                            dict = wrapper.Conditionals;
-                        else if (rt == typeof(void) || rt == typeof(Task))
-                            dict = wrapper.Actions;
-
-                        Debug.Assert(!isEvent || dict != wrapper.Conditionals, "Event has conditional!");
-                        Debug.Assert(!dict.ContainsKey(memberAttr.Name),
-                            $"Duplicate interface member: {memberAttr.Name}");
-
-                        // Build the function info and function parameter dictionary.
-                        var parameters = new List<(string, string)>();
-                        foreach (ParameterInfo parameter in method.GetParameters())
-                            parameters.Add((parameter.Name, parameter.ParameterType.Name));
-                        dict.Add(memberAttr.Name, new FunctionInfo
-                        {
-                            Info = method,
-                            Params = parameters.ToArray(),
-                            Attribute = memberAttr
-                        });
-                    }
-                }
+                    Info = method,
+                    Params = parameters.ToArray(),
+                    Attribute = memberAttr
+                });
             }
         }
 
@@ -138,8 +141,7 @@ namespace DiscordScriptBot.Script
         public static string GetInterfaceString(IWrapperInfo wrapper, string type)
         {
             var b = new StringBuilder();
-            b.AppendLine($"`{type}({wrapper.Name})`: {wrapper.Description}");
-            b.AppendLine("```");
+            b.AppendLine($"`{type}({wrapper.Name})`: {wrapper.Description}```");
             AppendFunctions(b, "Actions", wrapper.Actions);
             AppendFunctions(b, "Conditionals", wrapper.Conditionals);
             AppendFunctions(b, "Properties", wrapper.Properties);
