cbd52845acd24f085e4ef17f5dd6a510fd64ea07
Alexander Riddle <amrwf8@mail.missouri.edu>
2021-04-23 14:59:10 -0700

    Prototype script build commands

--- src/Command/ScriptBuildCommands.cs
+++ src/Command/ScriptBuildCommands.cs
@@ -1,5 +1,7 @@
 ﻿using Discord.Commands;
+using DiscordScriptBot.Builder;
 using DiscordScriptBot.Script;
+using DiscordScriptBot.Utility;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -7,62 +9,121 @@ namespace DiscordScriptBot.Command
 {
     public class ScriptBuildCommands : ModuleBase<CommandManager.CommandContext>
     {
+        private static ScriptBuilder _currentScript;
+
         [Command("newscript")]
         public async Task NewScript(string name, string eventTrigger)
         {
-            await Task.CompletedTask; // TODO
+            var @event = Context.ScriptInterface.GetEvent(eventTrigger);
+            if (@event == null)
+            {
+                await Context.Reply(nameof(NewScript), "Invalid event name.");
+                return;
+            }
+
+            _currentScript = new ScriptBuilder(name, null, @event);
+            await ReplyAsync($"Now building script: `{name}`, on event: `{eventTrigger}`.");
         }
 
         [Command("if")]
-        public async Task If(string expr, string then = null)
+        public async Task If(string @class, string func, params string[] @params)
         {
-            var arg = expr.Split(' ');
-            var @object = arg[0];
-            var @ref = Tokenize(@object, "[", "]");
+            if (!await CheckScriptInProgress())
+                return;
+            _currentScript.If(ResolveCall(@class, func, @params));
+            CheckThen(@params, true);
+        }
 
-            //b.SourceEvent.Properties.ContainsKey()
-            await Task.CompletedTask; // TODO
+        [Command("and")]
+        public async Task And(string @class, string func, params string[] @params)
+        {
+            if (!await CheckScriptInProgress())
+                return;
+            _currentScript.And(ResolveCall(@class, func, @params));
+            CheckThen(@params, true);
+        }
+
+        [Command("or")]
+        public async Task Or(string @class, string func, params string[] @params)
+        {
+            if (!await CheckScriptInProgress())
+                return;
+            _currentScript.Or(ResolveCall(@class, func, @params));
+            CheckThen(@params, true);
         }
 
         [Command("then")]
         public async Task Then()
         {
-            await Task.CompletedTask; // TODO
+            if (await CheckScriptInProgress())
+                _currentScript.Then();
         }
 
         [Command("else")]
         public async Task Else()
         {
-            await Task.CompletedTask; // TODO
+            if (await CheckScriptInProgress())
+                _currentScript.Else();
         }
 
         [Command("elif")]
-        public async Task ElseIf()
+        public async Task ElseIf(string @class, string func, params string[] @params)
         {
-            var b = new ScriptBuilder(null, null, null);
-            b.Else();
-            b.If();
-            await Task.CompletedTask; // TODO
+            if (!await CheckScriptInProgress())
+                return;
+            _currentScript.Else();
+            await If(@class, func, @params);
         }
 
         [Command("end")]
         public async Task End()
         {
-            //b.Pop();
-            await Task.CompletedTask; // TODO
+            if (await CheckScriptInProgress())
+                _currentScript.End();
         }
 
         [Command("action")]
-        public async Task Action(string stmt)
+        public async Task Action(string @class, string func, params string[] @params)
         {
-            await Task.CompletedTask; // TODO
+            if (await CheckScriptInProgress())
+                _currentScript.Action(ResolveCall(@class, func, @params));
         }
 
         [Command("done")]
         public async Task Done(string option = null)
         {
-            // option=="enable" then enable script immediately
-            await Task.CompletedTask; // TODO
+            if (!await CheckScriptInProgress())
+                return;
+            Context.ScriptManager.AddScript(_currentScript.Name,
+                                            _currentScript.Description,
+                                            Context.Guild.Id,
+                                            Context.User.Id.ToString(),
+                                            _currentScript.SourceEvent.Name,
+                                            _currentScript.Finish());
+
+            if (option != null && option.ToLower() == "enable")
+                Context.ScriptManager.ActivateScript(_currentScript.Name);
+
+            await ReplyAsync($"Script '{_currentScript.Name}' created.");
+            _currentScript = null;
+        }
+
+        private async Task<bool> CheckScriptInProgress()
+        {
+            if (_currentScript != null)
+                return true;
+            await ReplyAsync("No script being built!");
+            return false;
+        }
+
+        private bool CheckThen(string[] @params, bool doThen)
+        {
+            if (@params.Length == 0 || @params[@params.Length - 1].ToLower() != "then")
+                return false;
+
+            if (doThen)
+                _currentScript.Then();
+            return true;
         }
 
         private static string[] Tokenize(string src, string start, string end)
@@ -77,6 +138,27 @@ namespace DiscordScriptBot.Command
                 src = src.Substring(eIndex + end.Length);
             }
             return list.ToArray();
+        }
+
+        private ScriptBuilder.CallInfo ResolveCall(string @class, string func, params string[] @params)
+        {
+            var @ref = Tokenize(@class, "[", "]");
+            bool isRef = @ref.Length == 1;
+
+            // TODO: SUPPORT CALLING FUNCTIONS HERE
+            var actualParams = new List<IExpression>();
+            int offset = CheckThen(@params, false) ? 1 : 0;
+            for (int i = 0; i < @params.Length - offset; i++)
+                actualParams.Add(new ConstantExpression { Value = @params[i] });
+
+            return new ScriptBuilder.CallInfo
+            {
+                Ref = isRef ? @ref[0] : @class,
+                ParamName = !isRef ? @class : null,
+                ClassName = isRef ? @class.Substring(0, @class.IndexOf('[')) : null,
+                FuncName = func,
+                FuncParams = actualParams.ToArray()
+            };
         }
     }
 }
