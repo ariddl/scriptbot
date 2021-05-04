d81c5f3bbc6946d7ef9fc57af7a59d8583f0b0ed
Alexander Riddle <amrwf8@mail.missouri.edu>
2021-05-03 19:54:34 -0700

    Add comments to CommandManager

--- src/Command/CommandManager.cs
+++ src/Command/CommandManager.cs
@@ -17,10 +17,14 @@ namespace DiscordScriptBot.Command
         public class CommandContext : SocketCommandContext
         {
             private CommandManager _commandManager;
+
+            // Our context dictionary for quick access to instances for
+            // this user (i.e., a ScriptBuilder).
             private Dictionary<Type, object> Ctx
                 => _commandManager._userContext.ContainsKey(User.Id) ? 
                    _commandManager._userContext[User.Id] : null;
 
+            // Quick references for commands to use (no singletons currently!)
             public ScriptManager ScriptManager => _commandManager._scriptManager;
             public ScriptInterface ScriptInterface => _commandManager._scriptInterface;
 
@@ -33,8 +37,13 @@ namespace DiscordScriptBot.Command
 
             public void SetContext<T>(T value)
             {
+                // Check if we have a context dictionary for this user.
+                // If not, create one.
                 if (Ctx == null)
                     _commandManager._userContext.Add(User.Id, new Dictionary<Type, object>());
+
+                // If we don't already have context for this type, add it.
+                // Otherwise, overwrite it with the new instance.
                 if (GetContext<T>() == null)
                     Ctx.Add(typeof(T), value);
                 else
@@ -43,13 +52,21 @@ namespace DiscordScriptBot.Command
 
             public void RemoveContext<T>()
             {
+                // Do we have context for this type at all?
                 if (GetContext<T>() == null)
                     return;
+
+                // We have an instance of this type for this user, remove it!
+                // If this is the last element in our dictionary, remove it
+                // from the user context dictionary as well.
                 Ctx.Remove(typeof(T));
                 if (Ctx.Count == 0)
                     _commandManager._userContext.Remove(User.Id);
             }
 
+            // Try to get an instance of the type specified for this user.
+            // If one doesn't exist, return the default (which is typically
+            // a null reference).
             public T GetContext<T>() => Ctx != null ?
               (Ctx.ContainsKey(typeof(T)) ? 
               (T)Ctx[typeof(T)] : default(T)) : default(T);
@@ -58,6 +75,7 @@ namespace DiscordScriptBot.Command
                 => await Channel.SendMessageAsync($"`{cmd}`: {msg}");
         }
 
+        // The character that every command must start with.
         private const char PrefixChar = '/';
 
         private readonly DiscordSocketClient _client;
